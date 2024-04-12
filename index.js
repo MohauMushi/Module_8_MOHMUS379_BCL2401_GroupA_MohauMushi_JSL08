56d6219c2bbe9f1585efaf25bb603bbc46bc3037
Mohau Mushi <david1mushi1@gmail.com>
2024-04-12 10:20:41 +0200

    Renamed parameter(branckInfo -> branchInfo)

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ class BankBranch {
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
 //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
 //    - Return the `bankBranchInstance` whether it's newly created or existing.
-constructor(branckInfo){
+constructor(branchInfo){
 	if(!bankBranchInstance){
-		this.branckInfo = branckInfo;
+		this.branchInfo = branchInfo;
 		bankBranchInstance = this;
 	}
 	return bankBranchInstance;
 }
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
 	getBranchInfo(){
-		return this.branckInfo;
+		return this.branchInfo;
 	}
 }
 // 5. In the usage section:
